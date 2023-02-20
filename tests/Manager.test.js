1ef128386e1a569d9883702c695083916e73bd18
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2023-02-20 21:10:53 +1100

    Updated Manager test file

---
+++ tests/Manager.test.js
@@ -0,0 +1,23 @@
+const Manager = require("../lib/Manager");
+
+test("Create Office number Object", () => {
+    const testOfficeNumber = "800";
+    const manager = new Manager('Yoda', 888, 'yoda1@test.dev', testOfficeNumber);
+
+    expect(manager.officeNumber).toBe(testOfficeNumber);
+});
+
+test("getOfficeNumber() will return office number", () => {
+    const testOfficeNumber = "800";
+    const manager = new Manager('Yoda', 888, 'yoda1@test.dev', testOfficeNumber);
+
+    expect(manager.getOfficeNumber()).toBe(testOfficeNumber);
+});
+
+test("getRole() will return github", () => {
+    const role = "Manager";
+    const manager = new Manager('Yoda', 888, 'yoda1@test.dev', testOfficeNumber);
+
+    expect(manager.getRole()).toBe(role);
+});
+
