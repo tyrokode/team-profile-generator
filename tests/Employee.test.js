9a2e206dd969c8cc3fe2669090f6e894afed739c
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2023-02-20 21:09:52 +1100

    Updated Employee test file

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,1 +1,77 @@
-const Employee - require('../lib')
+const Employee = require('../lib/Employee');
+
+describe('Create Employee', () => {
+
+    it(" should create a new Employee", () => {
+        const employee = new Employee();
+        expect(typeof(employee)).toBe("object");
+    });
+}
+
+)
+
+describe('Name', () => {
+    it("should return the employee name", () => {
+        const name = "Grogu";
+        const employee = new Employee(name);
+    
+        expect(employee.name).toBe(name);
+    });
+})
+
+describe('ID', () => {
+    it("should return the employee Id", () => {
+        const id = "88";
+        const employee = new Employee(id);
+    
+        expect(employee.id).toBe(id);
+    });
+})
+
+describe('Email', () => {
+    it("should return employee Email", () => {
+        const email = "Grogu@test.dev";
+        const employee = new Employee(email);
+    
+        expect(employee.email).toBe(email);
+    });
+})
+
+
+describe('getName', () => {
+
+    it(" should get a name through getName()", () => {
+        const testName = "Mando"
+        const employee = new Employee(testName);
+        expect(employee.getName()).toBe(testName);
+    });
+}
+
+)
+
+describe('getID', () => {
+    it("should get an ID through getID()", () => {
+        const testId = "10";
+        const employee = new Employee(testId);
+    
+        expect(employee.getID()).toBe(testId);
+    });
+})
+
+describe('getEmail', () => {
+    it("should get an email through getEmail()", () => {
+        const testEmail = "Mando@test.dev";
+        const employee = new Employee(testEmail);
+    
+        expect(employee.getEmail()).toBe(testEmail);
+    });
+})
+
+describe('getRole', () => {
+    it("should return a role ", () => {
+        const testRole = "Employee";
+        const employee = new Employee(testRole);
+    
+        expect(employee.getRole).toBe(testRole);
+    });
+})
